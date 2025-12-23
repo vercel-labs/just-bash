77a49b8fcb639df879949740ac3bc112f06675b0
Malte Ubl <malte.ubl@gmail.com>
2025-12-23 14:27:37 -0800

    lint2

---
+++ vitest.comparison.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    include: ["src/comparison-tests/**/*.test.ts"],
+    exclude: ["**/node_modules/**", "**/dist/**"],
+  },
+});
