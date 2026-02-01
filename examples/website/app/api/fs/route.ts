b4fbfbc89adffc0220ee226d328b2391ffe5ee7b
Malte Ubl <malte.ubl@gmail.com>
2026-01-31 20:37:07 -0800

    Fix static build

--- examples/website/app/api/fs/route.ts
+++ examples/website/app/api/fs/route.ts
@@ -2,8 +2,6 @@ import { readdir, readFile } from "fs/promises";
 import { dirname, join, relative } from "path";
 import { fileURLToPath } from "url";
 
-export const dynamic = "force-static";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const AGENT_DATA_DIR = join(__dirname, "../agent/agent-data");
 
