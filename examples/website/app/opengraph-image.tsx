11b22a6a7f5bfc43fb871bc10465e46382f738e8
mamuso <mamuso@mamuso.net>
2026-05-05 16:02:47 -0700

    Format

--- examples/website/app/opengraph-image.tsx
+++ examples/website/app/opengraph-image.tsx
@@ -6,98 +6,100 @@ export const contentType = "image/png";
 
 const ASCII_ART = `   _           _   _               _
   (_)_   _ ___| |_| |__   __ _ ___| |__
-  | | | | / __| __| "_ \\ / _\` / __| "_ \\
+  | | | | / __| __| '_ \\ / _\` / __| '_ \\
   | | |_| \\__ \\ |_| |_) | (_| \\__ \\ | | |
  _/ |\\__,_|___/\\__|_.__/ \\__,_|___/_| |_|
-|__/`
+|__/`;
 
 export default async function Image() {
   const font = await fetch(
     new URL("https://fonts.gstatic.com/s/ibmplexmono/v19/-F63fjptAgt5VM-kVkqdyU8n5ig.ttf")
-  ).then((res) => res.arrayBuffer())
+  ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
-    <div
-      style={{
-        background: "#000",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-        fontFamily: "IBM Plex Mono",
-        padding: "60px 200px",
-      }}
-    >
-      <div style={{ display: "flex", marginLeft: "-135px", marginTop: "10px" }}>
-        <svg width="56" height="49" viewBox="0 0 112 98" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M56 0L112 98H0L56 0Z" fill="white" />
-        </svg>
-      </div>
-
-      <pre
-        style={{
-          color: "#fff",
-          fontSize: "32px",
-          lineHeight: "1.2",
-          margin: "-50px 0 0 0",
-          whiteSpace: "pre",
-        }}
-      >
-        {ASCII_ART}
-      </pre>
+    (
       <div
         style={{
+          background: "#000",
+          width: "100%",
+          height: "100%",
           display: "flex",
           flexDirection: "column",
           alignItems: "flex-start",
-          marginTop: "40px",
-          gap: "12px",
+          justifyContent: "flex-start",
+          fontFamily: "IBM Plex Mono",
+          padding: "60px 200px",
         }}
       >
+        <div style={{ display: "flex", marginLeft: "-135px", marginTop: "10px" }}>
+          <svg width="56" height="49" viewBox="0 0 112 98" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M56 0L112 98H0L56 0Z" fill="white" />
+          </svg>
+        </div>
+
+        <pre
+          style={{
+            color: "#fff",
+            fontSize: "32px",
+            lineHeight: "1.2",
+            margin: "-50px 0 0 0",
+            whiteSpace: "pre",
+          }}
+        >
+          {ASCII_ART}
+        </pre>
         <div
           style={{
-            color: "#7d7d7d",
-            fontSize: "31px",
-            lineHeight: "1.2",
-            textAlign: "center",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          A sandboxed bash interpreter for AI agents.
-          <br />
-          Pure TypeScript with in-memory filesystem.
-        </div>
-        <div
-          style={{
-            display: "flex",
+            alignItems: "flex-start",
+            marginTop: "40px",
             gap: "12px",
-            marginTop: "20px",
-            fontSize: "31px",
-            lineHeight: "1.2",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            flexGrow: 1,
-            width: "800px",
           }}
         >
-          <span style={{ color: "#FFF" }}>$</span>
-          <span
+          <div
             style={{
-              color: "#0AC5B3",
+              color: "#7d7d7d",
               fontSize: "31px",
+              lineHeight: "1.2",
+              textAlign: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
             }}
           >
-            npm install just-bash
-          </span>
+            A sandboxed bash interpreter for AI agents.
+            <br />
+            Pure TypeScript with in-memory filesystem.
+          </div>
+          <div
+            style={{
+              display: "flex",
+              gap: "12px",
+              marginTop: "20px",
+              fontSize: "31px",
+              lineHeight: "1.2",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              flexGrow: 1,
+              width: "800px",
+            }}
+          >
+            <span style={{ color: "#FFF" }}>$</span>
+            <span
+              style={{
+                color: "#0AC5B3",
+                fontSize: "31px",
+              }}
+            >
+              npm install just-bash
+            </span>
+          </div>
         </div>
       </div>
-    </div>,
+    ),
     {
       ...size,
       fonts: [
